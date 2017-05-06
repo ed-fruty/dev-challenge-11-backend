8f0acf698750625e306533372ae989da871031c1
Fruty <ed.fruty@gmail.com>
2017-05-06 22:50:45 +0300

    Adopts action and repository to specifications.

--- app/Common/Document/App/Http/Actions/GetDocumentListAction.php
+++ app/Common/Document/App/Http/Actions/GetDocumentListAction.php
@@ -4,8 +4,15 @@ namespace App\Common\Document\App\Http\Actions;
 
 
 use App\Common\Document\App\Http\Requests\GetDocumentListRequest;
+use App\Common\Document\App\Http\Responders\GetDocumentListResponder;
+use App\Common\Document\Concern\Specifications\StatusSpecification;
 use App\Common\Document\Concern\Traits\DocumentRepositoryAware;
+use App\Common\Document\Contracts\DocumentInterface;
 use App\Common\Document\Contracts\DocumentRepositoryAwareInterface;
+use App\Common\Specifications\Shared\CollectionsSpecification;
+use App\Common\Specifications\Shared\PaginatedSpecification;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\Paginator;
 
 class GetDocumentListAction implements DocumentRepositoryAwareInterface
 {
@@ -13,23 +20,22 @@ class GetDocumentListAction implements DocumentRepositoryAwareInterface
 
     /**
      * @param GetDocumentListRequest $request
-     * @param GetDocumentListResponder $responder
+     * @return Collection|Paginator|DocumentInterface[]
      */
-    public function __invoke(GetDocumentListRequest $request, GetDocumentListResponder $responder)
+    public function __invoke(GetDocumentListRequest $request)
     {
         $specifications = $this->buildSpecifications($request);
 
-        $documents = $this->documentRepository->match();
+        return $this->documentRepository->match($specifications);
     }
 
     /**
      * @param GetDocumentListRequest $request
-     * @return SpecificationCollection
+     * @return CollectionsSpecification
      */
     protected function buildSpecifications(GetDocumentListRequest $request)
     {
-        $collection = new SpecificationCollection();
-        $collection->push(new PaginedSpecification());
+        $collection = new CollectionsSpecification();
 
         if ($request->hasStatus()) {
             $collection->push(new StatusSpecification($request->getStatus()));
--- app/Common/Document/Contracts/DocumentRepositoryInterface.php
+++ app/Common/Document/Contracts/DocumentRepositoryInterface.php
@@ -2,6 +2,9 @@
 namespace App\Common\Document\Contracts;
 
 use App\Common\Document\Concern\ValueObjects\DocumentId;
+use App\Common\Specifications\Contracts\SpecificationInterface;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 interface DocumentRepositoryInterface
@@ -24,4 +27,10 @@ interface DocumentRepositoryInterface
      * @return DocumentFactoryInterface
      */
     public function getDocumentFactory() : DocumentFactoryInterface;
+
+    /**
+     * @param SpecificationInterface $specification
+     * @return mixed|Collection|Paginator|DocumentInterface[]
+     */
+    public function match(SpecificationInterface $specification);
 }
